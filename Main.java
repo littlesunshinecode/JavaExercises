f011abdf946da94dac28a16bf1f521e5fb7c3f19
littlesunshinecode <126683430+littlesunshinecode@users.noreply.github.com>
2023-03-28 10:19:33 -0300

    Update Main.java
    fixation of Java exercises. -Udemy

--- Main.java
+++ Main.java
@@ -16,8 +16,8 @@ public class Main {
 
 
                 double price1 = 2100.0; 
-double price2 = 650.50;
-double measure = 53.234567;                
+                double price2 = 650.50;
+                double measure = 53.234567;                
                 
 System.out.println("Products:");
 System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
